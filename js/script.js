75be3602e177e9d443a28bd783cf00be5e20ff2c
raffaelepiscitelli <raffaelepiscitelli1994@gmail.com>
2023-11-10 18:06:28 +0100

    Eserczio completo

--- js/script.js
+++ js/script.js
@@ -3,11 +3,14 @@ const passengerAge = parseInt(prompt("Type the passenger's age"));
 const ticketPricePerKm = 0.21;
 const minorAge = passengerAge < 18;
 const seniorAge = passengerAge > 65;
-const ticketPrice = numberKm * ticketPricePerKm;
+let ticketPrice = numberKm * ticketPricePerKm;
 const minorAgeDiscount = ticketPrice * 20 / 100;
 const seniorAgeDiscount = ticketPrice * 40 / 100;
-if (minorAge) {ticketPrice - minorAgeDiscount};
-if (seniorAge) {ticketPrice - seniorAgeDiscount};
+if (minorAge) {ticketPrice = ticketPrice - minorAgeDiscount};
+if (seniorAge) {ticketPrice = ticketPrice - seniorAgeDiscount};
+ticketPrice = ticketPrice.toFixed(2);
 document.getElementById("ticket-price").innerHTML = "The ticket price is "+ticketPrice+"€";
+
+
 
 
